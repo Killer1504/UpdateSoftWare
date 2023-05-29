4b9559becef71a0b694beeffd780713b83e092e1
Killer1504 <45095085+Killer1504@users.noreply.github.com>
2023-05-29 15:11:30 +0700

    render client: version 1.8.4.0  ---------------  render server: 1.4.0.0

--- push.bat
+++ push.bat
@@ -1,3 +1,3 @@
 git add .
-git commit -m "render client: version 1.8.4.0  ---------------  render server: 1.2.13.1"
+git commit -m "render client: version 1.8.4.0  ---------------  render server: 1.4.0.0"
 git push
