5844ec222e9891f26a790c29b4a211cf88797257
Killer1504 <45095085+Killer1504@users.noreply.github.com>
2023-06-14 10:58:22 +0700

    render client: version 1.9.5.0  ---------------  render server: 1.6.11.0

--- push.bat
+++ push.bat
@@ -1,3 +1,3 @@
 git add .
-git commit -m "render client: version 1.9.5.0  ---------------  render server: 1.6.10.0"
+git commit -m "render client: version 1.9.5.0  ---------------  render server: 1.6.11.0"
 git push
